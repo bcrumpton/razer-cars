d56eed818d054454575b7f739ab480b86be4a938
Brannon Crumpton <bcrump@daniel-johnsons-macbook-pro.local>
2017-03-23 16:10:05 -0500

    Structured and styled list of cars for brand

--- app/router.js
+++ app/router.js
@@ -9,6 +9,9 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('manufacturer', { path: 'manufacturers'}, function() {
     this.route('new');
+    this.route('car', { path: ':id' }, function() {
+      this.route('new');
+    });
   });
 });
 
