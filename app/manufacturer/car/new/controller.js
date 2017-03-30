9fc8c730e8739661b2cc8ff7086e6991d7601bc8
Brannon Crumpton <bcrump@daniel-johnsons-macbook-pro.local>
2017-03-29 20:22:07 -0500

    Tried to switch manufacturer/car/new to Ember data instead of fetch

--- app/manufacturer/car/new/controller.js
+++ app/manufacturer/car/new/controller.js
@@ -2,24 +2,17 @@ import Ember from 'ember';
 import config from 'razer-cars/config/environment';
 
 export default Ember.Controller.extend({
-  formValues: {
-    name: '',
-    year: '',
-    hp: '',
-    price: ''
-  },
+  formValues: {},
 
   actions: {
-    save() {
-      fetch(`${config.apiUrl}/cars`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json', accept: 'application/json' },
-        body: JSON.stringify({...this.formValues, brand_id: this.model.id}),
-      }).then(r => r.json())
-        .then(data => {
-          this.transitionToRoute('manufacturer.car.index');
-          this.set('formValues', {});
-        });
+    saveCar() {
+      const car = this.store.createRecord('car', this.formValues);
+      car.save().then(() => {
+        this.set('formValues', {});
+
+        this.transitionToRoute('manufacturer.car.index');
+      });
+
     },
   },
 });
