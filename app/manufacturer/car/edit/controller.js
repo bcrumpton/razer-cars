096ce48833b7c8bb992d2432a353b98f82e72cda
Brannon Crumpton <bcrumpton72@gmail.com>
2017-04-18 23:03:09 -0500

    Added edit route for editing and deleting cars

--- app/manufacturer/car/edit/controller.js
+++ app/manufacturer/car/edit/controller.js
@@ -1,16 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  formValues: {},
 
   actions: {
-    saveCar() {
-      const car = this.store.createRecord('car', this.formValues);
+    updateCar() {
+      const car = this.model;
       car.save().then(() => {
         this.set('formValues', {});
-
         this.transitionToRoute('manufacturer.car.index');
       });
     },
-  },
+
+    deleteCar() {
+      this.model.destroyRecord().then(() => {
+        this.transitionToRoute('manufacturer.car.index');
+      });
+    }
+  }
+
 });
--- app/manufacturer/car/index/route.js
+++ app/manufacturer/car/index/route.js
@@ -3,6 +3,9 @@ import config from 'razer-cars/config/environment';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('car');
+    const id = this.paramsFor('manufacturer.car').id;
+
+    return fetch(`${config.apiUrl}/brands/${id}`)
+      .then(r =>r.json());
   }
 });
